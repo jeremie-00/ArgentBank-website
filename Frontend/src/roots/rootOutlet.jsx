0b5cae0563faa74194a0af743783375a5b328da8
jeremie <jeremie.herault@orange.fr>
2024-04-11 12:13:11 +0200

    uploade footer

--- Frontend/src/roots/rootOutlet.jsx
+++ Frontend/src/roots/rootOutlet.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 import Header from "@layouts/header";
+import Footer from "@layouts/footer";
 
 export default function RootOutlet() {
     return <>
         <Header />
         <Outlet />
-
+        <Footer />
     </>
 }
