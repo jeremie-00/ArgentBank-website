199edd800666f79876f23779b4615a621c2ea151
jeremie <jeremie.herault@orange.fr>
2024-04-17 10:41:50 +0200

    config redux

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+//redux
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from "./reducers"
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
