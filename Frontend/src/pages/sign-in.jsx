7243909dce12936c8f0bec3eb1f63cbd558c6ca0
jeremie <jeremie.herault@orange.fr>
2024-04-23 10:45:56 +0200

    update sign-in

--- Frontend/src/pages/sign-in.jsx
+++ Frontend/src/pages/sign-in.jsx
@@ -1,33 +1,28 @@
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 
-import { checkAuthorization } from "../redux/actions/checkAutorization";
-import { login } from "../redux/reducers/loginReducer";
-import { setError, clearError } from "../redux/reducers/errorReducer";
+import { fetchLoginToken } from "../redux/actions/fetchAPI";
 
 export default function SignIn() {
     const [email, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const { isLoggedIn, isError, messageError } = useSelector((state) => state.user)
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        dispatch(checkAuthorization({ email, password }))
-            .then((response) => {
-                const token = response.payload.body.token
-                dispatch(login(token))
-                dispatch(clearError())
-                navigate("/user")
-            })
-            .catch((error) => {
-                console.error("Erreur lors de l'autorisation :", error.message)
-                dispatch(setError("Une erreur s'est produite"))
-            })
+        dispatch(fetchLoginToken({ email, password }))
     }
-    const isError = useSelector((state) => state.error.isError)
-    const messageError = useSelector((state) => state.error.message)
+    useEffect(() => {
+        if (isLoggedIn) {
+          navigate('/user')
+        } else {
+          navigate('/sign-in')
+        }
+      }, [isLoggedIn])
+    
 
     return (
         <main className="main bg-dark">
