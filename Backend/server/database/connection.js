2eba6a98d80d3670ceaa274eec549c865c602675
jeremie <jeremie.herault@orange.fr>
2024-04-25 11:31:24 +0200

    swagger transactions

--- Backend/server/database/connection.js
+++ Backend/server/database/connection.js
@@ -4,7 +4,7 @@ const databaseUrl =
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true, useFindAndModify: false })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
